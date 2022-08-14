df7fe3a3f2ff6ac4163f060b16f8a996eac2d002
rahulavashist <111230814+rahulavashist@users.noreply.github.com>
2022-08-14 18:10:06 +0530

    Practice

---
+++ practice1.js
@@ -0,0 +1,77 @@
+// // // // function sum_of_two_numbers(a,b)
+// // // // {
+// // // //     return(b-a)
+// // // // }
+
+// // // // const car={Name:"Audi",price:"100",color:"black"}
+// // // // console.log(car["Name"])
+// // // // const properties_array= Object.values(car)
+// // // // console.log(properties_array)
+// // // // const keys_array= Object.keys(car)
+// // // // console.log("keys=",keys_array)
+// // // // console.log("length of keys array",keys_array.length)
+// // // // // console.log(properties_array[0]+"kkk")
+// // // // // console.log(properties_array[1]+"kssdkk")
+// // // // // console.log(properties_array[2]+"sskkk")
+
+// // // // for(let i=0;i<keys_array.length;i++)
+// // // // {
+// // // //     console.log(properties_array[i]+"kkk" )
+// // // // }
+
+// // // // const person = {
+// // // //   firstName: "Ram",
+// // // //   lastName: "sham",
+// // // // age:25,
+// // // // eyeColor:"red",
+// // // // fullName : function( ){
+
+// // // //     return this["firstName"]+ " "+ this.lastName;
+// // // // }
+// // // // };
+// // // // console.log(person.fullName())
+
+// // // // module.exports ={
+// // // //     "sum_of_two_numbers":sum_of_two_numbers
+// // // // }
+// // // const person ={
+// // //     firstName:"ram",
+// // //     lastName:"sham",
+// // //     age:56,
+// // //     fullName : function(){
+// // //         return this.firstName + " " + this.lastName;
+// // //     console.log("hii")
+// // //     }
+// // // }
+// // // console.log(person.fullName())
+
+// // let text="dbjjrbuvbgurjbv".length
+
+// // console.log(text)
+// let text = "We are the so-called \"Vikings\" from the north.";
+// console.log(text)
+
+// let str = "Apple, Banana, Kiwi";
+// console.log(str.slice(-12))
+
+// let str = "wellcome to goa";
+// console.log(str.replace("wellcome", "hello"));
+// console.log(str.replace(/hello/i, "hello"));
+function multiply_of_two_number(a,b)
+{
+    return a*b
+}
+console.log("x=",multiply_of_two_number(12,5)) 
+ module.exports ={
+"multiply_of_two_number":multiply_of_two_number
+ }
+
+
+
+
+
+
+
+
+
+
